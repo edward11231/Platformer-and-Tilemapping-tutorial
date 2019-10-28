2e6bc2528205d2198aa0dad8c9f1024d8a0865a1
edward11231 <tavarez92@gmail.com>
2019-10-27 21:36:06 -0400

    Jump Adjustment

--- Platformer and Tilemapping Tutorial/Assets/Scripts/CameraScript.cs
+++ Platformer and Tilemapping Tutorial/Assets/Scripts/CameraScript.cs
@@ -15,13 +15,6 @@ public class CameraScript : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     { 
-        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);
-
-        //if (Input.GetKeyDown(KeyCode.S))
-            //this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y - cameraDown, this.transform.position.z);
-
-        //if (Input.GetKeyUp(KeyCode.S))
-            //this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y + cameraDown, this.transform.position.z);
-            
+        this.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, this.transform.position.z);        
     }
 }
